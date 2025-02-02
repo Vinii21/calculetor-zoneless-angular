20d7cf6743693bd0adeeb6ba44c4fda3618e8e27
Vinicio Chavarría U <viniciochavarriaugalde@gmail.com>
2025-02-02 14:45:13 -0600

    Pruebas unitarias para vericar atributos, clases de elementos HTML

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -39,4 +39,31 @@ describe('AppComponent', () => {
   it('should render router-outlet', () => {
     expect(compiled.querySelector('router-outlet')).not.toBeNull;
   });
+
+  it('should render router-outlet wrapped with css clases', () => {
+    const divElement = compiled.querySelector('div');
+    const mustHaveClasses = 'min-w-screen min-h-screen bg-slate-600 flex items-center justify-center px-5 py-5'.split(' ');
+    expect(divElement).not.toBeNull();
+    const divClasses = divElement?.classList.value.split(' ');
+
+    // Manera para evaluar clases especificas en el elemento HTML sin importar que tenga otras.
+    mustHaveClasses.forEach( className => {
+      expect(divClasses).toContain(className);
+    });
+
+    // Manera para evaluar que el elemento tenga clases especificas, si se le agrega otra, no pasa la prueba.
+    /* divElement?.classList.forEach((className)=>{
+      expect(mustHaveClasses).toContain(className);
+    }); */
+
+  });
+
+  it('should contain the "buy me a beer" link', ()=>{
+    const anchorElement = compiled.querySelector('a');
+    expect(anchorElement).not.toBeNull();
+    const titleAnchorAttribute = anchorElement?.title;
+    const hrefAttribute = anchorElement?.getAttribute('href');
+    expect(titleAnchorAttribute).toBe('Buy me a beer');
+    expect(hrefAttribute).toBe('https://www.buymeacoffee.com/scottwindon');
+  });
 });
